1cbd210f566de9d00877ff955e6e1b677b70138b
Jordan Baron <jordanbaron33@gmail.com>
2019-10-16 20:22:09 -0400

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,7 +1,7 @@
 from sb import Skyblock
 
 
-skyblock = Skyblock('')
+skyblock = Skyblock('') # tfw you forget to remove ur api key and have to regenerate it
 uuid = skyblock.uname_resolver('Technoblade')
 
 profiles = skyblock.get_player_profiles(uuid)
