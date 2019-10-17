214dd3250a8c124eaaec9054fd5d26577f0869a6
Jordan Baron <jordanbaron33@gmail.com>
2019-10-16 20:21:31 -0400

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,7 +1,7 @@
 from sb import Skyblock
 
 
-skyblock = Skyblock('4ba41725-11ee-44d9-969a-cc57ec771da8')
+skyblock = Skyblock('')
 uuid = skyblock.uname_resolver('Technoblade')
 
 profiles = skyblock.get_player_profiles(uuid)
